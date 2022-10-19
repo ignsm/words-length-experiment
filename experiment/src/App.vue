df7c80323ed5421feb4370b033dde4822230fef8
Ignat <mail@ignat.co.uk>
2022-10-19 21:58:41 +0200

    Added test

--- experiment/src/App.vue
+++ experiment/src/App.vue
@@ -1,23 +1,52 @@
 <script>
-function showWord(context, word) {
-  context.currentWord = word;
+function shuffle(array) {
+  let newArray = array.slice();
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
 }
+
+function showWord(context, word) {
+  context.wordToShow = word;
+}
+
 function showWords(words) {
   let that = this;
+  this.currentRoundWords = shuffle(words);
   for (let i = 0; i < words.length; i++) {
-    setTimeout(showWord, 100 * i, that, words[i]);
+    setTimeout(showWord, 1000 * i, that, words[i]);
   }
-  setTimeout(showWord, 600, that, "");
+  setTimeout(() => { showWord(that, ''); this.testIsActive = true }, 6000);
   if (this.roundsPassed < this.maxRounds) this.roundsPassed++;
 }
+
+function addRoundResult(word) {
+  if (this.currentTestResult.includes(word)) return;
+  this.currentTestResult.push(word);
+  if (this.currentTestResult.length == this.currentRoundWords.length) {
+    console.log('round finished');
+    this.testIsActive = !this.testIsActive;
+    this.experimentData.push(this.currentTestResult);
+    this.currentTestResult = [];
+    // TODO: save data
+    return;
+  }
+}
+
 export default {
   name: 'App',
   data() {
     return {
       words: [],
-      currentWord: '',
+      currentRoundWords: [],
+      wordToShow: '',
       maxRounds: 1,
       roundsPassed: 0,
+      testIsActive: false,
+      currentTestResult: [],
+      experimentData: [],
     }
   },
   created() {
@@ -25,24 +54,50 @@ export default {
     fetch(getWords)
       .then(response => response.json())
       .then(response => {
-        this.words = response.data.slice(0, 3);
-        // this.maxRounds = response.data.length;
-        this.maxRounds = 3;
+        this.words = response.data;
+        this.maxRounds = response.data.length;
       })
   },
   methods: {
     showWords,
-  }
+    addRoundResult,
+  },
 }
 </script>
 
 
 <template>
   <div class="content">
-    <p>{{currentWord?.toUpperCase()}}</p>
+    <p>{{wordToShow?.toUpperCase()}}</p>
 
-    <div class="btn-wrapper" :style="{display: currentWord == '' && roundsPassed < maxRounds ? 'block' : 'none'}">
-      <button @click="showWords(words[roundsPassed])">{{ roundsPassed > 0 ? 'Continue' : 'Start' }}</button>
+    <div class="btn-wrapper" v-if="wordToShow == '' && roundsPassed < maxRounds && !testIsActive">
+      <div v-if="roundsPassed == 0">
+        <p class="experiment-description">
+          Hello there!<br />You know, I'm something of a scientist myself. So let's do some
+          experiments!<br />{{maxRounds}} rounds left.
+        </p>
+      </div>
+      <div v-else>
+        <p class="experiment-description">
+          Round {{roundsPassed + 1}} from {{maxRounds}}!<br />Ready?
+        </p>
+      </div>
+      <button @click="showWords(words[roundsPassed])">Start</button>
+    </div>
+
+    <div class="test" id="test" v-if="testIsActive">
+      <p class="test-question">Click one by one on the words in the order they were presented:</p>
+      <div class="test__item" v-for="word in currentRoundWords" :key="word" @click="addRoundResult(word)">
+        <p class="word">{{word.toUpperCase()}}</p>
+        <p class="selected-number">
+          {{ currentTestResult.find((item, index) => item == word) ? currentTestResult.findIndex((item, index) => item == word) + 1 :
+          '' }}
+        </p>
+      </div>
+    </div>
+
+    <div v-if="wordToShow == '' && roundsPassed == maxRounds">
+      Thank you for participating! I didn't save any data yet, but I will in the future.
     </div>
   </div>
 </template>
@@ -57,5 +112,44 @@ export default {
   font-family: 'Arial', sans-serif;
   font-size: 24px;
   text-align: center;
+}
+
+.experiment-description {
+  font-size: 18px;
+  line-height: 140%;
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 18px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgb(0, 145, 255);
+  color: #fff;
+  cursor: pointer;
+}
+
+.test {
+  width: 600px;
+  text-align: left;
+}
+
+.test__item {
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  margin: 4px 0;
+}
+
+.test__item:hover {
+  background-color: #eee;
+}
+
+.test__item p {
+  line-height: 100% !important;
 }
 </style>
