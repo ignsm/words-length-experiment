a93a23ffb87db465e4556c1065e20e4e3af544b9
Ignat <mail@ignat.co.uk>
2022-10-24 18:19:36 +0200

    Added participants form and disclaimer

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,11 @@ function addRoundResult(word) {
   }
 }
 
+function nextStep() {
+  this.showStepOne = !this.showStepOne;
+  this.showStepTwo = !this.showStepTwo;
+}
+
 function saveData() {
   const api = 'https://upf-experiment.ignat.co.uk/save';
   const data = this.experimentData;
@@ -97,6 +102,29 @@ function generateApiUrl() {
   return resultApi;
 }
 
+function saveFormData() {
+  const data = this.experimentData
+  function isFieldExist(field) {
+    if (Object.prototype.hasOwnProperty.call(data, field)) {
+      return true;
+    }
+    return false;
+  }
+
+  if (
+    !isFieldExist('name')
+    && !isFieldExist('email')
+    && !isFieldExist('age')
+    && !isFieldExist('gender')
+    && !isFieldExist('langLvl')
+    && !isFieldExist('eduLvl')
+  ) {
+    alert('Please fill in all fields');
+    return;
+  }
+  this.showForm = false;
+}
+
 export default {
   name: 'App',
   data() {
@@ -111,8 +139,15 @@ export default {
       currentTestStartTime: 0,
       currentTestResult: [],
       currentTestTime: [],
-      experimentData: {},
+      experimentData: {
+        gender: "",
+        langLvl: "",
+        eduLvl: "",
+      },
       isExperimentFinished: false,
+      showForm: true,
+      showStepOne: true,
+      showStepTwo: false,
     }
   },
   created() {
@@ -129,12 +164,78 @@ export default {
     startNewRound,
     addRoundResult,
     saveData,
+    saveFormData,
+    nextStep,
   },
 }
 </script>
 
 
 <template>
+  <div class="initial-form" v-if="showForm">
+    <div class="consent-text step1" v-if="showStepOne">
+      <h2>Welcome to our experiment!</h2>
+      <p>We are three students enrolled in the CSIM master at the Universitat Pompeu Fabra in Barcelona. We have to
+        carry out a study as a homework assignment for the subject Research Methodologies in Humanities and Sciences. In
+        the following pages we will ask you to complete a series of word orders. You may choose to stop answering them
+        at any time and the data from incomplete questionnaires will not be used.</p>
+      <p>We would like to inform you that the recipients of the data will only be the students in question, and they
+        will not transfer this data to any entity, except to the tutors of the project, if absolutely necessary.
+        Likewise, the data will be anonymous at all times.</p>
+      <p>We understand that if you continue with the questionnaire from this point you accept these conditions and give
+        your consent.</p>
+      <p>If you have questions about the project you can contact us through any of the following emails:</p>
+      <ul>
+        <li>Ignat Smirnov: <a href="mailto:ignat.smirnov01@estudiant.upf.edu">ignat.smirnov01@estudiant.upf.edu</a></li>
+        <li>Cansu Ak: <a href="mailto:cansu.ak01@estudiant.upf.edu">cansu.ak01@estudiant.upf.edu</a></li>
+        <li>Ivan Zaitsev: <a href="mailto:ivan.zaitsev01@estudiant.upf.edu">ivan.zaitsev01@estudiant.upf.edu</a></li>
+      </ul>
+      <button class="btn-submit" type="button" @click.prevent="nextStep()">
+        Accept and continue
+      </button>
+    </div>
+
+    <div class="step2" v-if="showStepTwo">
+      <form>
+        <h2>Please, fill this form</h2>
+        <input type="text" name="name" id="name" placeholder="Your name" class="form-input"
+          v-model="experimentData.name">
+        <input type="email" name="email" id="email" placeholder="Your email" class="form-input"
+          v-model="experimentData.email">
+        <input type="number" name="age" id="age" placeholder="Your age" class="form-input" v-model="experimentData.age">
+        <select name="gender" id="gender" class="form-select" placeholder="Your gender" required
+          v-model="experimentData.gender">
+          <option value="" disabled selected>Your gender</option>
+          <option value="m">Male</option>
+          <option value="f">Female</option>
+          <option value="o">Other</option>
+          <option value="n">I don't want to share</option>
+        </select>
+        <select name="languageLevel" id="languageLevel" class="form-select" required
+          placeholder="Select your English language level" v-model="experimentData.langLvl">
+          <option value="" disabled selected>Select your English language level</option>
+          <option value="a1">A1 (beginner)</option>
+          <option value="a2">A2 (elementary)</option>
+          <option value="b1">B1 (intermediate)</option>
+          <option value="b2">B2 (upper intermediate)</option>
+          <option value="c1">C1 (advanced)</option>
+          <option value="c2">C2 (proficient)</option>
+          <option value="fluent">Fluent</option>
+        </select>
+        <select name="educationLevel" id="educationLevel" class="form-select" placeholder="Select your education level"
+          required v-model="experimentData.eduLvl">
+          <option value="" disabled selected>Select your education level</option>
+          <option value="bachelor">Bachelor</option>
+          <option value="master">Master</option>
+          <option value="phd">PhD</option>
+        </select>
+        <button class="btn-submit" type="button" @click.prevent="saveFormData()">
+          Submit form and start the experiment
+        </button>
+      </form>
+    </div>
+  </div>
+
   <div class="content">
     <p>{{wordToShow?.toUpperCase()}}</p>
 
@@ -175,6 +276,86 @@ export default {
 </template>
 
 <style>
+.initial-form {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.initial-form h2 {
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+}
+
+.form-input {
+  display: block;
+  margin: 10px auto;
+  padding: 10px;
+  width: 420px;
+  max-width: 100%;
+  font-size: 16px;
+  border: 1px solid rgba(60, 60, 60, 0.26);
+  ;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+.form-select {
+  display: block;
+  margin: 10px auto;
+  padding: 10px;
+  width: 420px;
+  max-width: 100%;
+  border: 1px solid rgba(60, 60, 60, 0.26);
+  border-radius: 5px;
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+.form-select:required:invalid {
+  color: #666;
+}
+
+option[value=""][disabled] {
+  display: none;
+}
+
+option {
+  color: #000;
+}
+
+.btn-submit {
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  max-width: 100%;
+}
+
+.consent-text {
+  margin: 0 auto;
+  width: 640px;
+  max-width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.consent-text button {
+  margin-top: 32px;
+}
+
+.consent-text p,
+.consent-text ul {
+  font-family: 'Arial', sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+}
+
 .content {
   height: 100vh;
   display: flex;
@@ -193,8 +374,8 @@ export default {
 
 button {
   margin-top: 20px;
-  padding: 10px 20px;
-  font-size: 18px;
+  padding: 12px 24px;
+  font-size: 16px;
   border: none;
   border-radius: 5px;
   background-color: rgb(0, 145, 255);
